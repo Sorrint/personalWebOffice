206e81b66159618b76f2811fd7950155e32c2080
Lebedev Anton <aslebedevdev@gmail.com>
2023-11-08 21:27:38 +0800

    Added profile menu
    - переработан header, удалены лишние классы
    - добавлено меню по нажатию на имя пользователя со ссылкой на Профиль
      и возможностью выхода из системы

--- src/widgets/header/components/header.tsx
+++ src/widgets/header/components/header.tsx
@@ -1,32 +1,53 @@
 import { NavLink } from 'react-router-dom';
-import { memo, useContext } from 'react';
+import { memo, useContext, useRef, useState } from 'react';
 
 import { IconFont } from '@shared/ui/iconFont';
 import { AuthContext } from '@shared/lib/context/authContext';
 import { Button } from '@shared/ui/button';
 import { useLogout } from '@features/logout';
+import { Popover } from '@shared/ui/popover';
 
 import styles from './header.module.scss';
 
 export const Header = memo(() => {
-    const {name} = useContext(AuthContext)
 
+    const {name} = useContext(AuthContext)
     const {logout} = useLogout()
-    
+    const refEl = useRef<HTMLDivElement>(null)
+    const [isOpen, setIsOpen] = useState<boolean>(false)
+
+    const setPopover = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (    
         <div className={styles.header}>
             <div className={styles.burger}>
                 <span></span>
             </div>
-            <div className={styles.logo}></div>
-            <span className={styles.text}>Кабинет</span>
-            <Button onClick={logout}> Выйти</Button>
-            {/* <div className={styles.icon}></div> */}
-            <IconFont iconName='icon-profile' classname={styles.icon}/>
-            <NavLink className={styles.mail} to={'/office/profile'}>
-                {name}
-            </NavLink>
- 
+            <div className={styles['logo-group']}>
+                <div className={styles.logo}></div>
+                <span className={styles['logo-text']}>Кабинет</span>
+            </div>
+            <div className={styles['profile-group']}>
+                <IconFont iconName='icon-profile' classname={styles.icon}/>
+                <div ref={refEl} onClick={setPopover} className={styles.mail}>
+                    {name}
+                </div>
+            </div>
+
+            {refEl  && refEl.current && 
+            <Popover isOpen={isOpen} onClose={()=> setIsOpen(false)} referenceElement={refEl.current as HTMLElement} key={'profile'}> 
+                <div className={styles.menu} onClick={()=> setIsOpen(false)}>
+                    <div className={styles['menu-item']} >
+                        <NavLink  to={'/office/profile'}>
+                            Профиль
+                        </NavLink>
+                    </div>
+                    <div onClick={logout} className={styles['menu-item']}>Выйти</div>
+                </div>
+            </Popover>
+            }
         </div>
     );
 });
