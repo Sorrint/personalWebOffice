6c519adb0b207578aff405000e547f71b57fa8d1
Lebedev Anton <aslebedevdev@gmail.com>
2023-11-22 14:07:53 +0800

    Fixed sidebar collapsing button

--- src/widgets/sideBar/components/sideBar/sideBar.tsx
+++ src/widgets/sideBar/components/sideBar/sideBar.tsx
@@ -8,6 +8,7 @@ import { sideBarScope } from '../../model/slices/menuItems';
 import { MenuItem } from '../menuItem/menuItem';
 import styles from './sideBar.module.scss';
 import { UserSettingsContext } from '@shared/lib/context/settingsContext';
+import { IconFont } from '@shared/ui/iconFont';
 
 export const SideBar = memo(() => {
     const {sidebar} = useContext(UserSettingsContext)
@@ -26,13 +27,20 @@ export const SideBar = memo(() => {
     })
 
     return (
-        <nav className={classNames(styles.sidenav, {[styles.collapsed]: collapsed})}>
-            {sideBarScope.map((item: IMenuItems) =>
-                <ul className={getSectionStyles(item.grow)} key={item.section}>
-                    {menuItems(item.items)}
-                </ul>
-            )}
-            <Button onClick={changeSidebar} full>{collapsed ? '>>' : '<<'}</Button>
-        </nav>
+        <div className={classNames(styles.sidebar,{[styles.collapsed]: collapsed} )}>
+            <nav className={classNames(styles.sidenav)}>
+                {sideBarScope.map((item: IMenuItems) =>
+                    <ul className={getSectionStyles(item.grow)} key={item.section}>
+                        {menuItems(item.items)}
+                    </ul>
+                )}
+            </nav>
+            <Button onClick={changeSidebar} classname={styles.button}>
+                {collapsed 
+                    ? <IconFont iconName='icon-chevron-right'/>
+                    : <IconFont iconName='icon-chevron-left'/>
+                }
+            </Button>
+        </div>
     );
 });
