3389d00655d5335bdb129f3d1a30a5237de6cf8e
Anton Lebedev <sorrint@gmail.com>
2024-01-09 13:51:42 +0800

    Изменил компонент CounterField
    - внес правки в компонент CounterField, поменяв его на forwardRef;
    - изменил файл стилей компонента;
    - исправил файл popupProductCard, используя измененный компонент CounterField

--- src/shared/ui/counterField/counterField.tsx
+++ src/shared/ui/counterField/counterField.tsx
@@ -1,67 +1,51 @@
-import { type MouseEvent, type ChangeEvent, type KeyboardEvent, type FocusEvent } from 'react';
-import { type UseFormRegister, type FieldValues, type Path } from 'react-hook-form';
+import {
+  type MouseEvent,
+  type ChangeEvent,
+  type KeyboardEvent,
+  type FocusEvent,
+  memo,
+  forwardRef,
+  type ForwardedRef,
+} from 'react';
+import { type FieldValues, type Path } from 'react-hook-form';
+import classNames from 'classnames';
 
-import './counterField.scss';
+import style from './counterField.module.scss';
 
 interface ICounterFieldsProps<T extends FieldValues> {
   label: string;
   name: Path<T>;
-  error?: string;
-  placeholder?: string;
-  register?: UseFormRegister<T>;
-  formName?: string;
-  autoComplete?: string;
-  field?: T;
-  value?: string;
   onClick?: (e: MouseEvent) => void;
   onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
-  inputClass?: string;
+  classname?: string;
   onKeyPress?: (e: KeyboardEvent, name: Path<T>) => void;
   tabIndex?: number;
   onFocus?: (e: FocusEvent<HTMLInputElement>) => void;
 }
 
-export function CounterField<T extends FieldValues>(props: ICounterFieldsProps<T>) {
-  const {
-    label,
-    name,
-    error,
-    register,
-    autoComplete,
-    field,
-    value,
-    onChange,
-    onClick,
-    inputClass,
-    onKeyPress,
-    tabIndex,
-    onFocus,
-  } = props;
-  const getInputClasses = () => {
-    return (
-      'counter-container__input' +
-      (error ? ' is-invalid' : '') +
-      (inputClass ? ` ${inputClass}` : '')
-    );
-  };
+export const CounterField = memo(
+  forwardRef(function CounterField<T extends FieldValues>(
+    props: ICounterFieldsProps<T>,
+    ref: ForwardedRef<HTMLInputElement | null>,
+  ) {
+    const { label, name, onChange, onClick, classname, onKeyPress, tabIndex, onFocus } = props;
+    const inputClass = classNames(style.input, classname);
 
-  return (
-    <div className='counter-container'>
-      <div className='counter-group has-validation'>
-        <label className='counter-label' htmlFor={name}>
-          {label}
-        </label>
+    return (
+      <div className={style.component}>
+        {label && (
+          <label className={style.label} htmlFor={name}>
+            {label}
+          </label>
+        )}
         <input
           id={name}
           name={name}
-          className={getInputClasses()}
-          {...(register && { ...register(name, { valueAsNumber: true }) })}
-          autoComplete={autoComplete}
-          {...field}
+          className={inputClass}
           onChange={onChange}
-          value={value}
           onClick={onClick}
           type={'number'}
+          ref={ref}
           onKeyDown={
             onKeyPress &&
             ((e) => {
@@ -72,7 +56,6 @@ export function CounterField<T extends FieldValues>(props: ICounterFieldsProps<T
           onFocus={onFocus}
         />
       </div>
-      {error && <div className='invalid-feedback'>{error}</div>}
-    </div>
-  );
-}
+    );
+  }),
+);
