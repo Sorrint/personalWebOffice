f21396d901a8d39646e26a42a503a169c1ff5b29
Anton Lebedev <aslebedevdev@gmail.com>
2024-01-21 13:31:16 +0800

    Добавил функционал обновления в editOrderingRows
    - добавил иконки для действий;
    - привязал компонент counterField к react-hook-form

--- src/features/editOrdreringRows/components/editOrderingRows.tsx
+++ src/features/editOrdreringRows/components/editOrderingRows.tsx
@@ -2,10 +2,15 @@ import { memo } from 'react';
 import style from './editOrderingRows.module.scss';
 import { HPopover } from '@shared/ui/popover';
 import { IconFont } from '@shared/ui/iconFont';
-import { useAppDispatch } from '@shared/lib/hooks';
-import { orderingActions } from '@entities/orderings';
+import { useAppDispatch, useAppSelector } from '@shared/lib/hooks';
+import {
+  type IOrderingChapter,
+  getOrderingChaptersData,
+  orderingActions,
+} from '@entities/orderings';
 import classNames from 'classnames';
 import { CounterField } from '@shared/ui/counterField';
+import { useForm } from 'react-hook-form';
 
 interface EditOrderingRowsProps {
   classname?: string;
@@ -14,12 +19,17 @@ interface EditOrderingRowsProps {
 
 export const EditOrderingRows = memo(({ chapterKey, classname }: EditOrderingRowsProps) => {
   const dispatch = useAppDispatch();
-  const editRows = (key: string) => {
-    dispatch(
-      orderingActions.setChapterSummary({
-        [key]: { rowsCount: 2 },
-      }),
-    );
+  const chapters = useAppSelector(getOrderingChaptersData);
+
+  const { register, handleSubmit } = useForm<IOrderingChapter['summary']>({
+    defaultValues: {
+      rowsCount: chapters?.[chapterKey]?.summary.rowsCount ?? 0,
+    },
+    mode: 'onChange',
+  });
+
+  const editRows = (key: Partial<IOrderingChapter['summary']>) => {
+    dispatch(orderingActions.setChapterSummary({ [chapterKey]: key }));
   };
 
   return (
@@ -30,7 +40,18 @@ export const EditOrderingRows = memo(({ chapterKey, classname }: EditOrderingRow
       placementPanel='right'
       offsetY={10}
     >
-      <CounterField name='rows' min={0}></CounterField>
+      <form onSubmit={handleSubmit(editRows)} className={style.form}>
+        <CounterField
+          min={0}
+          autofocus
+          {...register('rowsCount', { valueAsNumber: true })}
+        ></CounterField>
+        <div className={style.actions}>
+          <IconFont iconName={'icon-check'} classname={style.check} />
+          <IconFont iconName={'icon-close'} classname={style.close} />
+          <IconFont iconName={'icon-cached'} classname={style.cached} />
+        </div>
+      </form>
     </HPopover>
   );
 });
--- src/features/saveOrderingToXLSX/components/saveOrderingToXLSX.tsx
+++ src/features/saveOrderingToXLSX/components/saveOrderingToXLSX.tsx
@@ -1,23 +1,27 @@
 import classNames from 'classnames';
 
-import { getOrderingData, getOrderingSummary } from '@entities/orderings';
+import { getOrderingChaptersData, getOrderingSummary } from '@entities/orderings';
 import { useAppSelector } from '@shared/lib/hooks';
 
 import { saveOrderingToExcel } from '../lib/helpers/';
 import style from './saveOrderingToXLSX.module.scss';
 
 interface SaveOrderingToXLSXProps {
-    classname?: string
+  classname?: string;
 }
-export const SaveOrderingToXLSX = ({classname}: SaveOrderingToXLSXProps) => {
-    const orderingData = useAppSelector(getOrderingData)
-    const summaryData = useAppSelector(getOrderingSummary)
+export const SaveOrderingToXLSX = ({ classname }: SaveOrderingToXLSXProps) => {
+  const orderingData = useAppSelector(getOrderingChaptersData);
+  const summaryData = useAppSelector(getOrderingSummary);
 
-    const saveToXLSX = () => {
-        if (orderingData && summaryData) {
-            saveOrderingToExcel({chaptersData: orderingData, summaryData}, 'Порядовка Demo')
-        }
-    };
+  const saveToXLSX = () => {
+    if (orderingData && summaryData) {
+      saveOrderingToExcel({ chaptersData: orderingData, summaryData }, 'Порядовка Demo');
+    }
+  };
 
-    return  <button className={classNames(style['create-xlsx'], classname)} onClick={saveToXLSX}>Скачать в формате XLSX</button>;
+  return (
+    <button className={classNames(style['create-xlsx'], classname)} onClick={saveToXLSX}>
+      Скачать в формате XLSX
+    </button>
+  );
 };
